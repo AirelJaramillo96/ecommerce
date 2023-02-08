3b4151bd4778f8294f2cf75534d60be86ee12dc3
AirelJaramillo96 <airel.jaramillo@cceo.com.mx>
2023-02-08 14:17:55 -0600

    class 6: cicle of life of angular

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import {Component, OnInit, Input, Output, EventEmitter, OnChanges, AfterViewInit, OnDestroy} from '@angular/core';
 
 @Component({
   selector: 'app-img',
@@ -10,10 +10,37 @@ export class ImgComponent implements OnInit {
   @Input() img: string = '';
   @Output() loaded = new EventEmitter<string>();
   imgDefault: string = './assets/images/descarga.png';
-  constructor() { }
+
+  constructor() {
+    //before render
+    //Not run async code here (like http request)  In vue.js is like created()
+    console.log('constructor', 'imgValue =>', this.img);
+  }
+
+  ngOnChanges() {
+    //before render
+    // inputs changed in vue.js is like watch
+    console.log('ngOnChanges', 'imgValue =>', this.img)
+  }
 
   ngOnInit(): void {
+    //before render
+    //Can run async code here (like http request) In vue.js is like created() this run once time
+    console.log('ngOnInit', 'imgValue =>', this.img);
   }
+
+  ngAfterViewInit() {
+    //after render
+    //In vue.js is like mounted handler children components
+    console.log('ngAfterViewInit', 'imgValue =>', this.img);
+  }
+
+  ngOnDestroy() {
+    //delete component
+    //In vue.js is like beforeDestroy
+    console.log('ngOnDestroy', 'imgValue =>', this.img);
+  }
+
   imgError() {
     this.img = this.imgDefault;
   }
