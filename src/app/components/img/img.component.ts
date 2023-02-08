4b5f93f6f52d20d0daa5052c20588ae1d81ffea7
AirelJaramillo96 <airel.jaramillo@cceo.com.mx>
2023-02-08 13:14:38 -0600

    pass values parent and child

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
-export class ImgComponent {
+export class ImgComponent implements OnInit {
+
+  @Input() img: string = 'https://www.w3schools.com/howto/img_avatar.png';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
 }
