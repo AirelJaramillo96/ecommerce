b369091d8101435446e5562884e51b4a1fb36c90
AirelJaramillo96 <airel.jaramillo@cceo.com.mx>
2023-02-15 13:16:01 -0600

    class 16: create user and login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { AuthService } from "./services/users/auth.service";
+import { UserService } from "./services/users/user.service";
+import {CreateUserDTO, User} from "./models/user.module";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,11 +13,34 @@ export class AppComponent {
   imgParent = '';
   showImg = true;
 
+  constructor(
+    private authService: AuthService,
+    private userService: UserService
+  ) {
+  }
+
   onLoaded(img: string) {
     console.log('img loaded in parent', img);
   }
 
   toggleImg() {
     this.showImg = !this.showImg;
+  }
+
+  createUser() {
+    let user: CreateUserDTO= {
+      name: 'John Marston',
+      email: 'johnmarston@gmail.com',
+      password: '123456'
+    }
+    this.userService.create(user).subscribe((user: User) => {
+      console.log('user created', user);
+    })
+  }
+
+  login() {
+    this.authService.login('johnmarston@gmail.com', '123456').subscribe(rta => {
+      console.log('rta login', rta.access_token);
+    });
   }
 }
---
+++ src/app/services/users/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from "../../../environments/environment";
+import {CreateUserDTO, User} from "../../models/user.module";
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+
+  private apiURL = `${environment.API_URL}/api/users`;
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  create(dto: CreateUserDTO) {
+    return this.http.post<User>(this.apiURL, dto);
+  }
+
+  getAll() {
+    return this.http.get<User[]>(this.apiURL);
+  }
+
+}
